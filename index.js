717cd4b9bdc456af00f5d83bb2a8b5ed2a0a33e1
marlontunchez@gmail.com <Marlon.1989>
2019-06-10 17:03:05 -0600

    change index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express= require("express");
 let app = express();
-let port = process.env.port|| 3000;
+let port = process.env.port || 3000;
 let cors= require("cors");
 let rindex= require("./src/routes/index.js");
 let morgan = require("morgan");
